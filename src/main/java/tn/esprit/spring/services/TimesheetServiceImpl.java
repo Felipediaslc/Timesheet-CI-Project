6f436d29d823bea94cc57db6455747449e57ead2
DriftMoon <bilel.amdouni.1@esprit.tn>
2020-10-28 21:09:08 +0100

    hudsonrules30

--- src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +30,10 @@ public class TimesheetServiceImpl implements ITimesheetService {
 	@Autowired
 	TimesheetRepository timesheetRepository;
 	@Autowired
-	EmployeRepository employeRepository;
+	EmployeRepository employeRepository; 
+
+	private static Logger log = Logger.getLogger(TimesheetServiceImpl.class);
+
 	
 	public int ajouterMission(Mission mission) {
 		missionRepository.save(mission);
@@ -60,12 +64,12 @@ public class TimesheetServiceImpl implements ITimesheetService {
 
 	
 	public void validerTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin, int validateurId) {
-		System.out.println("In valider Timesheet");
+		log.info("In valider Timesheet");
 		Employe validateur = employeRepository.findById(validateurId).get();
 		Mission mission = missionRepository.findById(missionId).get();
 		//verifier s'il est un chef de departement (interet des enum)
 		if(!validateur.getRole().equals(Role.CHEF_DEPARTEMENT)){
-			System.out.println("l'employe doit etre chef de departement pour valider une feuille de temps !");
+			log.info("l'employe doit etre chef de departement pour valider une feuille de temps !");
 			return;
 		}
 		//verifier s'il est le chef de departement de la mission en question
@@ -77,7 +81,7 @@ public class TimesheetServiceImpl implements ITimesheetService {
 			}
 		}
 		if(!chefDeLaMission){
-			System.out.println("l'employe doit etre chef de departement de la mission en question");
+			log.info("l'employe doit etre chef de departement de la mission en question");
 			return;
 		}
 //
@@ -87,7 +91,7 @@ public class TimesheetServiceImpl implements ITimesheetService {
 		
 		//Comment Lire une date de la base de données
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.println("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
+		log.info("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
 		
 	}
 
