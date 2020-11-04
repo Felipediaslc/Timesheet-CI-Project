8f90a43bce89bf3d4fe93d0b65c21b5d46776d2b
Nvx12 <raslensafsaf@gmail.com>
2020-11-04 22:34:25 +0100

    test Junit/Log4j

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -3,7 +3,7 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,51 +12,47 @@ import tn.esprit.spring.services.*;
 
 @SpringBootTest
 class TimesheetApplicationTests {
-
-	/*Entreprise em=new Entreprise("nokia","INFO" );
-	EntrepriseServiceImpl emp=new EntrepriseServiceImpl();
+	
 	private static final Logger l = Logger.getLogger(TimesheetApplicationTests.class);
 	@Autowired
-	IEntrepriseService entrepriseService;
-	@Test
-	void testAjoutEntrprise() {
+	IEmployeService employeService;
+	/*@Test
+	void testAjoutEmployee() {
 		int k=0;
 		try {
-			k=entrepriseService.ajouterEntreprise(em);
+			k=employeService.addOrUpdateEmploye(new Employe("safsaf", "raslen", "raslensafsaf@gmail.com", "test", false, Role.ADMINISTRATEUR));
 		boolean test=(k!=0)?true:false;
-		
-		assertEquals(test, true,"ajout avec success de l'entreprise");
-		
+		//JUnit test
+		assertEquals(test, true,"ajout avec success de lemployee");
+		//Log4j 
 		if(test) {
-			l.info("entreprise avec id "+k+" added successfully ");
+			l.info("employee avec id "+k+" added successfully ");
 		}
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 		}
-	}
+	}*/
 
 	@Test
-	void suppressionEntreprise() {
-		/*int nb_avans_suppression=entrepriseService.getEntrepriseById();*/
+	void suppressionEmployee() {
+		int nb_avans_suppression=employeService.getNombreEmployeJPQL();
 
-		/*try {
+		try {
 			System.out.println(nb_avans_suppression);
-			entrepriseService.deleteEntrepriseById(2);
-			int nb_apres_suppression=entrepriseService.getNombreEmployeJPQL();
+			employeService.deleteEmployeById(11);
+			int nb_apres_suppression=employeService.getNombreEmployeJPQL();
 			System.out.println(nb_apres_suppression);
-			boolean test=(nb_apres_suppression!=nb_avans_suppression)?true:false;*/
+			boolean test=(nb_apres_suppression!=nb_avans_suppression)?true:false;
 		
-		/*assertEquals(test, true,"supprimee  avec success de lemployee");
-		
+		assertEquals(test, true,"supprimee  avec success de lemployee");	
 		if(test) {
-			l.info("entreprise avec id "+2+" deleted  successfully ");
+			l.info("employee avec id "+11+" deleted  successfully ");
 		}
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 		}
 	}
-	
-*/
+
 }
